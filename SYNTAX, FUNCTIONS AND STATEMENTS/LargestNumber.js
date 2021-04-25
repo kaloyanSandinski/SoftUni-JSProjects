796296ecdf3b279f4bb3089ae9f002489204b3e6
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-04-25 16:00:23 +0300

    Added new problem: LargestNumber

---
+++ SYNTAX, FUNCTIONS AND STATEMENTS/LargestNumber.js
@@ -0,0 +1,6 @@
+function printLargest(firstNum, secondNum, thirdNum){
+    let largest = Math.max(firstNum, secondNum, thirdNum);
+    console.log(`The largest number is ${largest}.`);
+}
+
+printLargest(1,2,3);
