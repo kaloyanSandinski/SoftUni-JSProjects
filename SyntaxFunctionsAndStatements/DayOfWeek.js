4a9196327253b60dda77ebf4e771e5290f16daf8
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-04-27 20:54:12 +0300

    Added new problem: DayOfWeek

---
+++ SyntaxFunctionsAndStatements/DayOfWeek.js
@@ -0,0 +1,23 @@
+function printTheNumberOfDay(param) {
+    let result;
+    if (param == 'Monday') {
+        result = 1;
+    } else if (param == 'Tuesday') {
+        result = 2;
+    } else if (param == 'Wednesday') {
+        result = 3;
+    } else if (param == 'Thursday') {
+        result = 4;
+    } else if (param == 'Friday') {
+        result = 5;
+    } else if (param == 'Saturday') {
+        result = 6;
+    } else if (param == 'Sunday') {
+        result = 7;
+    } else {
+        result = 'error';
+    }
+    return result;
+}
+
+console.log(printTheNumberOfDay('Invalid'));
