ff861837466e084491a93d4f82f4fa46bd2b3d51
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-05-01 22:11:32 +0300

    Added new problem: GreatestCommonDivisor

---
+++ SyntaxFunctionsAndStatements/GreatestCommonDivisor.js
@@ -0,0 +1,13 @@
+function greatestDevisor(a, b){
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a; 
+    }
+}
+
+console.log(greatestDevisor(2154, 458));
