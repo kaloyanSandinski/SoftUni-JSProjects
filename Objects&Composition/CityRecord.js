fcfd636543a0755fcd910da9adf3f36db78920c8
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-05-28 15:29:14 +0300

    Added new problem: CityRecord

---
+++ Objects&Composition/CityRecord.js
@@ -0,0 +1,12 @@
+function solve(name, population, treasury) {
+    const city = {};
+    city.name = name;
+    city.population = population;
+    city.treasury = treasury;
+
+    return city;
+}
+
+console.log(solve('Tortuga',
+    7000,
+    15000));
