0bb6779d6e357ba0ff3e04ef9a0fd290df24bf6d
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-06-05 19:23:31 +0300

    EDITS

--- DocumentObjectModel/EditElement/EditElement.js
+++ DocumentObjectModel/EditElement/EditElement.js
@@ -1,4 +1,4 @@
 function editElement(reference, match, replacer) {
-    const output = reference.textContent.replace(new RegExp(match), replacer);
+    const output = reference.textContent.replace(new RegExp(match, 'g'), replacer);
     reference.textContent = output;
 }
