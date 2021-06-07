101f22cf94b0e25c1a2afa5be6be5a0dea3f96c0
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-06-07 19:43:05 +0300

    Added new problem: ShowMore

---
+++ DocumentObjectModel/ShowMore/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    let link = document.querySelector('a#more');
+    link.style.display = 'none';
+    let more = document.querySelector('span#text');
+    more.style.display = 'inline';
+}
