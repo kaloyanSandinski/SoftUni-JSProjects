177268714d2214406002d564a98db9294dc7ce63
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-07-10 15:50:37 +0300

    Added new problem: Github-Repos

---
+++ AsynchronousProgramming/Github-Repos/scripts/app.js
@@ -0,0 +1,31 @@
+function loadRepos() {
+    let button = document.querySelector('button');
+    button.addEventListener('click', asyncLoadRepos);
+}
+
+async function asyncLoadRepos() {
+    let username = document.querySelector('#username').value;
+    let reposList = document.querySelector('#repos');
+    let url = `https://api.github.com/users/${username}/repos`;
+    try {
+        const response = await fetch(url);
+        if (response.status == 404) {
+            reposList.innerHTML = '';
+            throw new Error('User not found!');
+        }
+        const data = await response.json();
+        reposList.innerHTML = '';
+        console.log('Promise fulfilled!');
+        data.forEach(r => {
+            let listItem = document.createElement('li');
+            let link = document.createElement('a');
+            link.href = r.html_url;
+            link.textContent = r.full_name;
+            listItem.appendChild(link);
+            reposList.appendChild(listItem);
+        });
+    } catch (error) {
+        console.log('Promise rejected!');
+        console.log(error.message);
+    }
+}
