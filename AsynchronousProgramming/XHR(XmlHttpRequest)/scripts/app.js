c1e124b15f321b2cdb47d7d974b952928fb0baad
Kaloyan Sandinski <kaloqnsimeonov1997@icloud.com>
2021-07-10 10:30:03 +0300

    Added new problem: XHR(XmlHttpRequest)

---
+++ AsynchronousProgramming/XHR(XmlHttpRequest)/scripts/app.js
@@ -0,0 +1,14 @@
+function loadRepos() {
+    let button = document.querySelector('body button');
+    button.addEventListener('click', () => {
+        let url = 'https://api.github.com/users/testnakov/repos';
+        let httpRequest = new XMLHttpRequest();
+        httpRequest.addEventListener('readystatechange', () => {
+            if (httpRequest.readyState == 4 && httpRequest.status == 200) {
+                document.querySelector('#res').textContent = httpRequest.responseText;
+            }
+        });
+        httpRequest.open('GET', url);
+        httpRequest.send();
+    });
+}
